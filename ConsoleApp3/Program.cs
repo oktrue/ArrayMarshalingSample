50afeacbf95ae418e5402b0f7ea7b57d06278a25
Олег Трушков <oleg_trushkov@hotmail.com>
2021-10-12 18:37:31 +0300

    Output transformation

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -5,19 +5,10 @@ namespace ConsoleApp3
     public class Program
     {
         [DllImport("TransformationEstimation")]
-        private static extern void EstimateTransformation([In, Out] float[,] points, int count, float tolerance = 10.0f);
+        private static extern IntPtr EstimateTransformation([In] float[,] points, int count, [Out] float[] transformation, float tolerance = 10.0f);
 
         public static void Main(string[] args)
         {
-            //var src = new float[5, 3]
-            //    {
-            //        { -52.0f, 63.11f, 0.28f },
-            //        { 60.000f, 10.042f, -5.530f },
-            //        { 0, 0, 0 },
-            //        { 20.000f, 80.042f, 2.530f },
-            //        { -10.00f, -13.000f, -2.030f }
-            //    };
-
             var src = new float[3, 3]
                 {
                     { 64.044304f, 2693.0754f, -170.95724f },
@@ -25,7 +16,10 @@ namespace ConsoleApp3
                     { 4917.587f, 1063.1492f, 863.1775f }
                 };
 
-            EstimateTransformation(src, src.GetLength(0));
+            var transformation = new float[6];
+
+            EstimateTransformation(src, src.GetLength(0), transformation);
+
             Console.ReadKey();
         }
     }
